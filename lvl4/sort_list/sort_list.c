b071394b8b52fda8dcc73f2cf34fd9a5e9429f49
rfulop <rfulop@student.42.fr>
2017-10-16 03:41:33 +0200

    added removeif sortlist fprime

--- lvl4/sort_list/sort_list.c
+++ lvl4/sort_list/sort_list.c
@@ -1,40 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_list_remove_if.c                                :+:      :+:    :+:   */
+/*   sort_list.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rfulop <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/10/16 03:01:41 by rfulop            #+#    #+#             */
-/*   Updated: 2017/10/16 03:10:54 by rfulop           ###   ########.fr       */
+/*   Created: 2017/10/16 03:29:57 by rfulop            #+#    #+#             */
+/*   Updated: 2017/10/16 03:37:02 by rfulop           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_list.h"
-#include <stdlib.h>
-#include <unistd.h>
+#include "list.h"
 
-void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+t_list *sort_list(t_list *lst, int (*cmp)(int, int))
 {
-	t_list *lst;
-	t_list *tmp;
+	t_list *begin;
+	int tmp;
 
-	while ((*begin_list) && !(cmp((*begin_list)->data, data_ref)))
-	{
-		tmp = *begin_list;
-		*begin_list = (*begin_list)->next;
-		free(tmp);
-	}
-	lst = *begin_list;
+	begin = lst;
 	while (lst && lst->next)
 	{
-		if (!cmp(lst->next->data, data_ref))
+		if (!(cmp(lst->data, lst->next->data)))
 		{
-			tmp = lst->next;
-			lst->next = tmp->next;
-			free(tmp);
+			tmp = lst->data;
+			lst->data = lst->next->data;
+			lst->next->data = tmp;
+			lst = begin;
 		}
-		if (lst->next)
+		else
 			lst = lst->next;
 	}
+	lst = begin;
+	return (lst);
 }
